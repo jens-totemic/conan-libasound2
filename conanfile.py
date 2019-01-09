e31596af7f57d42e80ca0b3674d82769417a4acc
Jens <jens@totemiclabs.com>
2019-01-08 22:18:45 -0800

    Improved logging

--- conanfile.py
+++ conanfile.py
@@ -12,15 +12,9 @@ class DebianDependencyConan(ConanFile):
     sha     = "8aa152b840021ab3fbebe2d099a0106f226eec92551c36ce41d5d3310a059849"
     sha_dev = "736d846de5bfcac933c9f35ac47b1e5f128901856ffce08f8865e8dfc8a15966"
     homepage = "http://www.alsa-project.org/"
-    description = "shared library for ALSA applications -- development files This package contains files required for developing software that makes use of libasound2, the ALSA library."
+    description = "shared library for ALSA applications -- development files. This package contains files required for developing software that makes use of libasound2, the ALSA library."
     url = "https://github.com/jens-totemic/conan-pjsip"    
     settings = "os", "arch"
-
-#     def requirements(self):
-#         if self.settings.os == "Linux":
-#             self.requires("libalsa/1.1.5@conan/stable")
-#         if self.options.SSL:
-#             self.requires(_openSSL+"/1.0.2@conan/stable")      
 
     def translate_arch(self):
         arch_string = str(self.settings.arch)
@@ -65,31 +59,23 @@ class DebianDependencyConan(ConanFile):
                         dest.append(full_entry[:-len(suffix_to_duplicate)])
 
     def package_info(self):
-        self.output.info("package_info")
         # we only need the autotool class to generate the host variable
         autotools = AutoToolsBuildEnvironment(self)
         # construct path using platform name, e.g. usr/lib/arm-linux-gnueabihf/pkgconfig
         pkgpath = "usr/lib/%s/pkgconfig" % autotools.host 
         pkgconfigpath = os.path.join(self.package_folder, pkgpath)
-        self.output.info(pkgconfigpath)
+        self.output.info("package info file: " + pkgconfigpath)
         with tools.environment_append({'PKG_CONFIG_PATH': pkgconfigpath}):
             pkg_config = tools.PkgConfig("alsa")
-            self.output.info(pkg_config.libs)
-            self.output.info(pkg_config.libs_only_L)
-            self.output.info(pkg_config.libs_only_l)
-            self.output.info(pkg_config.libs_only_other)
-            self.output.info(pkg_config.cflags)
-            self.output.info(pkg_config.cflags_only_I)
-            self.output.info(pkg_config.variables)
-        # The entries in the package file are not including the absolute path, add it here
-        self.copy_cleaned(pkg_config.libs_only_L, "-L", self.cpp_info.lib_paths, self.package_folder, None, [])
-        self.output.info(self.cpp_info.lib_paths)
-        
-        # exclude all libraries from dependencies here, they are separately included
-        self.copy_cleaned(pkg_config.libs_only_l, "-l", self.cpp_info.libs, "", None, [])
-        self.output.info(self.cpp_info.libs)
+            # The entries in the package file are not including the absolute path, add it here
+            self.copy_cleaned(pkg_config.libs_only_L, "-L", self.cpp_info.lib_paths, self.package_folder, None, [])
+            self.output.info("lib_paths %s" % self.cpp_info.lib_paths)
 
-        # when creating include paths, we need to add an additional include directory that does not end on /alsa,
-        # so #include<alsa/version.h> works
-        self.copy_cleaned(pkg_config.cflags_only_I, "-I", self.cpp_info.include_paths, self.package_folder, "/alsa", [])
-        self.output.info(self.cpp_info.include_paths)
+            # exclude all libraries from dependencies here, they are separately included
+            self.copy_cleaned(pkg_config.libs_only_l, "-l", self.cpp_info.libs, "", None, [])
+            self.output.info("libs: %s" % self.cpp_info.libs)
+
+            # when creating include paths, we need to add an additional include directory that does not end on /alsa,
+            # so #include<alsa/version.h> works
+            self.copy_cleaned(pkg_config.cflags_only_I, "-I", self.cpp_info.include_paths, self.package_folder, "/alsa", [])
+            self.output.info("include_paths: %s" % self.cpp_info.include_paths)
